872507781607838e8be65d99230dbe055c6257f4
Miooonggg <102677763+Miooonggg@users.noreply.github.com>
2023-09-01 19:03:21 +0800

    updated files

---
+++ javascript/app.js
@@ -0,0 +1,24 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry)
+      if(entry.isIntersecting)
+      {
+        entry.target.classList.add('show');
+      }
+      else
+      {
+        entry.target.classList.remove('show');
+      }
+    });
+  });
+
+  const hiddenElements = document.querySelectorAll('.welcome-div');
+  const hiddenElemental = document.querySelectorAll('.cards');
+  
+  hiddenElements.forEach((el) => observer.observe(el));
+  hiddenElemental.forEach((el) => observer.observe(el));
+
+
+
+
+
