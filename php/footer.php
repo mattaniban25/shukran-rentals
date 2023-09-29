c78e14c8bd1edf2404f14ba68d6dce02efe7ff2b
mattaniban25 <aniban.matthewemmanuel@ue.edu.ph>
2023-09-30 02:35:40 +0800

    aaa

---
+++ php/footer.php
@@ -0,0 +1,62 @@
+<?php
+
+    echo '
+    <div class="footer">
+      <div class="bg-dark border-top border-2 text-light border-danger" style="box-shadow: black 0px 0px 10px 0px;">
+        <div class="container p-4 pb-0">
+          <section class="">
+            <div class="row">
+              <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3 text-center justify-content-center">
+                <img src="../IMAGES/logo.png" alt="" height="125">
+              </div>
+
+              <hr class="w-100 clearfix d-md-none" />
+
+              <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 class="text-uppercase mb-4 font-weight-bold">Useful Links</h6>
+                <p><a class="text-white" href="#">Home</a></p>
+                <p><a class="text-white" href="#">About Us</a></p>
+                <p><a class="text-white" href="#">Facilities & Amenities</a></p>
+              </div>
+
+              <hr class="w-100 clearfix d-md-none" />
+
+              <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 class="text-uppercase mb-4 font-weight-bold"><p>&nbsp;</p></h6>
+                <p><a class="text-white" href="#">Photo Gallery</a></p>
+                <p><a class="text-white" href="#">Contact Us</a></p>
+                <p><a class="text-white" href="#">Terms & Conditions</a></p>
+              </div>
+
+              <hr class="w-100 clearfix d-md-none" />
+
+              <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+                <p><i class="fas fa-home mr-3"></i> 3384 Arias Compound, Parañaque, 1701 Metro Manila</p>
+                <p><i class="fas fa-phone mr-3"></i> +63 998 991 7073</p>
+                <p><i class="fas fa-envelope mr-3"></i> shukranrentalsopc@gmail.com	</p>
+              </div>
+            </div>
+          </section>
+
+          <hr class="my-3">
+
+          <section class="p-3 pt-0">
+            <div class="row d-flex align-items-center">
+              <div class="col-md-7 col-lg-8 text-center text-md-start">
+                <div class="p-3">
+                  <p>© 2023 | Shukran Rentals OPC</p>
+                </div>
+              </div>
+              <div class="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
+                <a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button"><i class="fab fa-facebook-f"></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button"><i class="fab fa-instagram"></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button"><i class="fab fa-google"></i></a>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>  
+    ';
+?>
