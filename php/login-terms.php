160c59079e42f6514f96271b76c06e8077d699e0
mattaniban25 <aniban.matthewemmanuel@ue.edu.ph>
2023-11-07 23:49:40 +0800

    martin

--- php/login-terms.php
+++ php/login-terms.php
@@ -9,3 +9,131 @@
     $result = mysqli_query($conn, $sql);
     $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="ANIBAN, MATTHEW EMMANUEL DJ." />
+
+    <!-- social links logo -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
+    
+    <!-- bootleg -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+    <!-- CSS FILE -->
+    <link rel="stylesheet" href="../css/style.css">
+
+    <title>Shukran Rentals - Login</title>
+
+  </head>
+
+  <body>
+
+    <script>
+      AOS.init();
+    </script>
+    
+    <?php include 'login-header.php'; ?> 
+
+    <main>
+
+        <div class="container mt-5" align="center">
+            <h1 class="fs-2">TERMS AND CONDITIONS</h1>
+            <hr style="width:50%;">
+        </div>
+
+
+        <br>
+
+        <div class="container" style="width:90%;">
+          <div class="row">
+            <div class="col-lg-12">
+              <ol>
+                    <li style="list-style-type:decimal;">Pay as you Ckeck-in and Strictly</li>
+                    <li style="list-style-type:decimal;">Pay as you Order - Please pay food bill.</li>
+                    <li style="list-style-type:decimal;">Head Counts is being implemented.</li>
+                    <li style="list-style-type:decimal;">
+                      The Management is Not Responsible for the safekeeping of any
+                      personal belongings or valuables.
+                    </li>
+                    <li style="list-style-type:decimal;">
+                      Banning of illegal drugs, illegal gambling and other illegal
+                      activities are strictly observed. Firearms and ammunition
+                      are Not Allowed.
+                    </li>
+                    <li style="list-style-type:decimal;">
+                      All units are presented in Good Condition. Please
+                      immediately upon check-in, for any other issues. Rooms and
+                      Facilities are provided as is. Any damages that may occur,
+                      will be CHARGED ACCORDINGLY.
+                    </li>
+                    <li style="list-style-type:decimal;">
+                      Please do not bring materials not limited to decors and
+                      equipment that are owned by the property. Otherwise, it is
+                      right to consider such item as sold and must be Paid by the
+                      Guest.
+                    </li>
+                    <li style="list-style-type:decimal;">
+                      Violence and Misconduct of any sort will not be tolerated.
+                    </li>
+                    <li style="list-style-type:decimal;">
+                      Security Guard on Duty, reserves the RIGHT TO INSPECT any
+                      container, or closed packages that are visibly unique or
+                      deem suspicious which are being brought in and out of the
+                      building premises.
+                    </li>
+                    <li style="list-style-type:decimal;">
+                      Keeping the noise at minimum to avoid disturbing other
+                      Guests is at everyone's interests.
+                    </li>
+                    <li style="list-style-type:decimal;">
+                      The Management will not be liable for any damage or loss of
+                      a vehicle.
+                    </li>
+                    <li style="list-style-type:decimal;">
+                      Loitering in hallways (especially for children) and
+                      Vandalism within the premises are not allowed and offenders
+                      of such acts will be given proper sanctions.
+                    </li>
+                    <li style="list-style-type:decimal;">
+                      Tampering of Equipment's is considered a Criminal Act and
+                      will not be taken lightly by the Management.
+                    </li>
+                    <li style="list-style-type:decimal;">
+                      Strictly NO SMOKING inside the Rooms and triggering of Smoke
+                      Detector Alarm will result to a P 500.00 FINE or more. There
+                      is a Smoking Area specifically provided by the Management.
+                    </li>
+                    <li style="list-style-type:decimal;">
+                      Disposal of waste must be properly stowed in the provided
+                      Trash Bins.
+                    </li>
+                    <li style="list-style-type:decimal;">
+                      The Management reserves the Right to Vacate Rooms that
+                      exceeds reservation period.
+                    </li>
+                    <li style="list-style-type:decimal;">
+                      Room Keys are to be surrendered to Lobby Personnel or
+                      Security Guard on Duty upon Checking out. Lost Keys are
+                      charged for P 500.00 without exemption
+                    </li>
+                    <li style="list-style-type:decimal;">
+                      Occasional Events, Parties and other Celebratory Gatherings
+                      must follow the permissible 10:00 PM Barangay ordinance.
+                    </li>
+                    <li style="list-style-type:decimal;">
+                      For other Terms & Conditions apply. You can ask the Front
+                      desk Clerk for the full list.
+                    </li>
+              </ol>
+            </div>
+          </div>
+        </div>
+    </main>
+    <?php include 'login-footer.php'; ?>
+  </body>
+</html>
